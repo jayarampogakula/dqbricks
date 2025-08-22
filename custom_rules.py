a44f2aa8bfa3605010fb3075c48ba38f76e73ce6
jayarampogakula <109912693+jayarampogakula@users.noreply.github.com>
2025-08-22 12:24:43 +0530

    Create custom_rules.py

---
+++ custom_rules.py
@@ -0,0 +1,14 @@
+# custom_rules.py
+from rules.base import Rule
+from dqcore.registry import RuleRegistry
+from pyspark.sql.functions import col, length
+
+
+class MaxLength(Rule):
+NAME = 'max_length'
+def build(self):
+return length(col(self.params['col'])) <= int(self.params['max'])
+
+
+# Register
+RuleRegistry.register(MaxLength.NAME, MaxLength)
